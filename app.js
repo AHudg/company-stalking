f66c852e668cc7f10d8919f15561e7d7c617a0ab
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-08-17 14:49:29 -0500

    Finished adding roles and employees functionality

--- app.js
+++ app.js
@@ -19,60 +19,174 @@ const renderInformation = function(queryID) {
 };
 
 const addEmployee = function() {
+    const employeeInfoArray = [];
 
+    let sqlRoleData = selectQuery(1);
+
+    db.query(sqlRoleData, (err, roleResults) => {
+        if (err) {
+            console.log('ERROR 500. We apologize! Looks like there is an error on our end. Check back in with us later!');
+            return;
+        };
+    
+        const roleArray = roleResults.map(({ title }) => (title));
+        const roleDeptID = roleResults.map(({ dept_id }) => (dept_id));
+
+        let sqlEmpData = selectQuery(0);
+
+        db.query(sqlEmpData, (err, empResults) => {
+             if (err) {
+                console.log('ERROR 500. We apologize! Looks like there is an error on our end. Check back in with us later!');
+                return;
+            };
+
+            const empArray = empResults.map(({ first_name, last_name }) => (first_name + " " + last_name));
+            const empManagerID = empResults.map(({ emp_id }) => (emp_id));
+
+            inquirer
+                .prompt([
+                    {
+                        type: 'text',
+                        name: 'first',
+                        message: 'What is the first name of the new employee?',
+                        validate: firstInput => {
+                            if (firstInput) {
+                                return true;
+                            };
+                            console.log('Please enter in the employee first name before proceeding.');
+                            return false;
+                        }
+                    },
+                    {
+                        type: 'text',
+                        name: 'last',
+                        message: 'What is the last name of the new employee?',
+                        validate: lastInput => {
+                            if (lastInput) {
+                                return true;
+                            };
+                            console.log('Please enter the last name of the employee before proceeding.');
+                            return false;
+                        }
+                    },
+                    {
+                        type: 'list',
+                        name: 'role',
+                        message: 'What role does this employee belong to?',
+                        choices: roleArray
+                    },
+                    {
+                        type: 'list',
+                        name: 'manager',
+                        message: 'Who is the manager for this employee?',
+                        choices: empArray
+                    }
+                ])
+                .then(({ first, last, role, manager}) => {
+                    employeeInfoArray.push(first);
+                    employeeInfoArray.push(last);
+
+                    for (let i = 0; i < roleArray.length; i++) {
+                        if (roleArray[i] === role) {
+                            const roleID = i + 1;
+                            employeeInfoArray.push(roleID);
+                            const deptID = roleDeptID[i];
+                            employeeInfoArray.push(deptID)
+                        };
+                    };
+
+                    for (let i = 0; i < empArray.length; i++) {
+                        if (empArray[i] === manager) {
+                            const managerID = i + 1;
+                            employeeInfoArray.push(managerID);
+                        };
+                    };
+
+                    const sql = selectQuery(5);
+
+                    db.query(sql, employeeInfoArray, (err, results) => {
+                        if (err) {
+                            console.log('ERROR 400. Looks like the request was bad. Try resubmitting and double check the input.');
+                            return;
+                        };
+                        console.log(`Successfully added ${employeeInfoArray[0]} ${employeeInfoArray[1]} to the available roles.\n`);
+                        return mainMenu();
+                    })
+
+                })
+        });
+    });
 };
 
 const addRole = function() {
     const roleInfoArray = [];
 
-    const sql = selectQuery(2);
+    const sqlDeptData = selectQuery(2);
 
-    db.query(sql, (err, results) => {
+    db.query(sqlDeptData, (err, results) => {
         if (err) {
             console.log('ERROR 500. We apologize! Looks like there is an error on our end. Check back in with us later!');
             return;
         };
 
-        const deptArray = results.map(({ dept_id, dept_name }) => (dept_name));
+        const deptArray = results.map(({ dept_name }) => (dept_name));
 
-    inquirer
-        .prompt([{
-                type: 'text',
-                name: 'role',
-                message: 'What is the name of the new role?',
-                validate: roleInput => {
-                    if (roleInput) {
-                        return true;
-                    };
-                    console.log('Please enter a department name to be added.');
-                    return false;
+        inquirer
+            .prompt([
+                {
+                    type: 'text',
+                    name: 'role',
+                    message: 'What is the name of the new role?',
+                    validate: roleInput => {
+                        if (roleInput) {
+                            return true;
+                        };
+                        console.log('Please enter a department name to be added.');
+                        return false;
+                    }
+                },
+                {
+                    type: 'number',
+                    name: 'salary',
+                    message: 'What is the salary associated with this role?',
+                    validate: salaryInput => {
+                        if (salaryInput) {
+                            return true;
+                        };
+                        console.log('Please enter a salary for this role.');
+                        return false;
+                    }
+                },
+                {
+                    type: 'list',
+                    name: 'department',
+                    message: 'What department does this role belong to?',
+                    choices: deptArray
                 }
-            },
-            {
-                type: 'text',
-                name: 'salary',
-                message: 'What is the salary associated with this role?',
-                validate: salaryInput => {
-                    if (salaryInput) {
-                        return true;
+            ])
+            .then(({ role, salary , department}) => {
+                roleInfoArray.push(role);
+                roleInfoArray.push(salary);
+
+                for (let i = 0; i < deptArray.length; i++) {
+                    if (deptArray[i] === department) {
+                        const departmentID = i + 1;
+                        roleInfoArray.push(departmentID);
                     };
-                    console.log('Please enter a salary for this role.');
-                    return false;
-                }
-            },
-            {
-                type: 'list',
-                name: 'department',
-                message: 'What department does this role belong to?',
-                choices: deptArray
-            }])
-        .then(({ role, salary , department}) => {
-            roleInfoArray.push(role);
-            roleInfoArray.push(salary);
-            roleInfoArray.push(department);
-            console.log( roleInfoArray);
+                };
+
+                const sql = selectQuery(6);
+
+                db.query(sql, roleInfoArray, (err, results) => {
+                    if (err) {
+                        console.log('ERROR 400. Looks like the request was bad. Try resubmitting and double check the input.');
+                        return;
+                    };
+                    console.log(`Successfully added ${roleInfoArray[0]} to the available roles.\n`);
+                    return mainMenu();
+                });
             });
-        })
+    });
 };
 
 const addDepartment = function() {
@@ -90,13 +204,13 @@ const addDepartment = function() {
             }
         })
         .then(({ department }) => {
-            const sql = `INSERT INTO department (dept_name) VALUES (?);`
+            const sql = selectQuery(7);
 
             db.query(sql, department, (err, results) => {
                 if (err) {
                     console.log('Error. Could not create the new department.');
                 };
-                console.log(`Added ${department} as a department.`);
+                console.log(`Successfully added ${department} as a department.\n`);
                 return mainMenu();
             });
         })
@@ -121,7 +235,7 @@ const mainMenu = function() {
         .then(({ menu }) => {
             switch(menu) {
                 case 'View All Employees':
-                    renderInformation(0);
+                    renderInformation(3);
                     break;
                 case 'Add Employee':
                     addEmployee();
@@ -130,7 +244,7 @@ const mainMenu = function() {
                     updateEmployeeRole();
                     break;
                 case 'View All Roles':
-                    renderInformation(1);
+                    renderInformation(4);
                     break;
                 case 'Add Role':
                     addRole();
